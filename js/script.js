3ec4b2c043025ba52c549af82d981bd9cf18d25c
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-01-30 12:39:25 +0100

    Ändrade så man ser ifall man har träffat med röd färg och gjort så att knappen disablas när man träffar.

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ console.log(enemyBattleShipLocation1[0], enemyBattleShipLocation1[1])
         console.log("Newlocation1y"+ " " + enemyBattleShipLocation1[1])
     }
 
+let points = 0;
 
 const battleShipLocations = document.querySelectorAll(".battleShipLocation");
 console.log(battleShipLocations);
@@ -27,13 +28,19 @@ function checkLocation() {
     console.log(location[1]);
     
     if (location[0] == enemyBattleShipLocation0[0] && location[1] == enemyBattleShipLocation0[1]) {
-        console.log("Hej")
-        
+        console.log("Hej, du träffade")
+        this.disabled = true;
+        points++;
     }
 
     if (location[0] == enemyBattleShipLocation1[0] && location[1] == enemyBattleShipLocation1[1]) {
-        console.log("Hej")
-        
+        console.log("Hej, du träffade")
+        this.disabled = true;
+        points++;
+    }
+
+    if (points == 2) {
+        console.log("Yay du vann du fick " + points + " poäng")
     }
 
 }
