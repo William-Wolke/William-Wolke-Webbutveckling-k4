b4a0db1204ff1b7a0742622aa3b016cdb5e24eaf
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-01-30 13:28:35 +0100

    Knapp som laddar om sidan och början på skuta på fel ruta

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let enemyBattleShipLocation0 = [Math.floor(Math.random() * 10) + 1,Math.floor(Ma
 console.log(enemyBattleShipLocation0[0], enemyBattleShipLocation0[1])
 
 let enemyBattleShipLocation1 = [Math.floor(Math.random() * 10) + 1,Math.floor(Math.random() * 10) + 1];
-console.log(enemyBattleShipLocation1[0], enemyBattleShipLocation1[1]) 
+console.log(enemyBattleShipLocation1[0], enemyBattleShipLocation1[1])
 
     if (enemyBattleShipLocation0[0] == enemyBattleShipLocation1[0]) {
         enemyBattleShipLocation1[0] = Math.floor(Math.random() * 10) + 1;
@@ -38,6 +38,18 @@ function checkLocation() {
         this.disabled = true;
         points++;
     }
+
+    if (location[0] != enemyBattleShipLocation0[0] || location[1] != enemyBattleShipLocation0[1]) {
+        console.log("Hej, du träffade")
+        this.disabled = true;
+        points++;
+    }
+
+    if (location[0] != enemyBattleShipLocation1[0] || location[1] != enemyBattleShipLocation1[1]) {
+        console.log("Hej, du träffade")
+        this.disabled = true;
+        points++;
+    }    
 
     if (points == 2) {
         console.log("Yay du vann du fick " + points + " poäng")
