632f27348922123faec4048f905d8d5742b7dab5
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-02-10 08:26:01 +0100

    någonting

--- js/script.js
+++ js/script.js
@@ -2,17 +2,30 @@ let enemyBattleShipLocation0 = [Math.floor(Math.random() * 10) + 1,Math.floor(Ma
 console.log(enemyBattleShipLocation0[0], enemyBattleShipLocation0[1])
 
 let enemyBattleShipLocation1 = [Math.floor(Math.random() * 10) + 1,Math.floor(Math.random() * 10) + 1];
-console.log(enemyBattleShipLocation1[0], enemyBattleShipLocation1[1])
 
     if (enemyBattleShipLocation0[0] == enemyBattleShipLocation1[0]) {
         enemyBattleShipLocation1[0] = Math.floor(Math.random() * 10) + 1;
-        console.log("Newlocation1x" + " " + enemyBattleShipLocation1[0])
+        
     }
 
     if (enemyBattleShipLocation0[1] == enemyBattleShipLocation1[1]) {
         enemyBattleShipLocation1[1] = Math.floor(Math.random() * 10) + 1;
-        console.log("Newlocation1y"+ " " + enemyBattleShipLocation1[1])
     }
+
+    console.log(enemyBattleShipLocation1[0] + " " + enemyBattleShipLocation1[1]);
+
+let enemyBattleShipLocation2 = [Math.floor(Math.random() * 10) + 1 , Math.floor(Math.random() * 10) + 1 ,1 ,1];
+
+let j = 0;
+
+for (let i = 2; i < 6; i++) {
+    j++;
+    enemyBattleShipLocation2[i] = enemyBattleShipLocation2[1] + j;
+}
+
+for (let i = 0; i < enemyBattleShipLocation2.length; i++) {
+    console.log(enemyBattleShipLocation2[0] + " " + enemyBattleShipLocation2[i]);
+}
 
 let points = 0;
 
@@ -33,34 +46,61 @@ function checkLocation() {
 
         this.disabled = true;
         
-        this.style.backgroundColor = "green";
+        /*this.style.backgroundColor = "green";*/
+
+        this.class = "hit";
         
         points++;
     }
-
     if (location[0] == enemyBattleShipLocation1[0] && location[1] == enemyBattleShipLocation1[1]) {
 
         console.log("Du träffade")
 
         this.disabled = true;
 
-        this.style.backgroundColor = "green";
+        /*this.style.backgroundColor = "green";*/
+
+        this.style.class = "hit";
         
         points++;
     }
-    
-    if (location[0] != enemyBattleShipLocation0[0] || location[1] != enemyBattleShipLocation0[1]) {
 
-        console.log("Du missade")
+    for (let i = 0; i < enemyBattleShipLocation2.length; i++) {
+        if (location[0] == enemyBattleShipLocation2[0] && location[0] == enemyBattleShipLocation2[i]) {
+
+        console.log("Du träffade")
 
         this.disabled = true;
+
+        /*this.style.backgroundColor = "green";*/
+
+        this.style.class = "hit";
+        
+        points++;
+        }
+    
     }
 
-    if (location[0] != enemyBattleShipLocation1[0] || location[1] != enemyBattleShipLocation1[1]) {
+    if (location[0] == enemyBattleShipLocation2[0] && location[1] == enemyBattleShipLocation2[1] || location[0] == enemyBattleShipLocation2[0] && location[1] == enemyBattleShipLocation2[1]) {
 
-        console.log("Du missade")
+        console.log("Du träffade")
 
         this.disabled = true;
+
+        /*this.style.backgroundColor = "green";*/
+
+        this.style.class = "hit";
+        
+        points++;
+    }
+
+
+    
+    else {
+
+        console.log("Du missade lmao sämst")
+        this.disabled = true;
+
     }  
 
     if (points == 2) {
@@ -68,6 +108,9 @@ function checkLocation() {
         console.log("Yay du vann du fick " + points + " poäng")
 
         document.getElementById('playAgain').style.display = "grid";
+
     }
 
+    
 }
+
